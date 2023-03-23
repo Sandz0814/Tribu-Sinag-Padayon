4e44053941d19396717525d373f1ba21c92bcafa
Sandz0814 <sandrojimena0814@gmail.com>
2023-03-23 19:49:05 +0800

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -1,18 +1,17 @@
 <?php
-
 if (isset($_POST['submit'])){
-    $Name = $POST['Name'];
-    $Phone = $POST['Phone'];
-    $Email = $POST['mail'];
-    $Subject = $POST['Subject'];
+    $Name = $_POST['Name'];
+    $Phone = $_POST['Phone'];
+    $Email = $_POST['Email'];
+    $Subject = $_POST['Subject'];
+    $Message = $_POST['Message'];
 
     $mailTo = "sandrojimena0814@gmail.com";
-    $headers = "From: ".$Email";
-    $txt = "You have receive an email from ".$Name.". \n\n.$
-
+    $headers = "From: ".$Email;
+    $txt = "You have received an email from ".$Name.".\n\nPhone: ".$Phone."\nEmail: ".$Email."\n\nMessage:\n".$Message;
 
     mail($mailTo, $Subject, $txt, $headers);
-    header("Location: Contact.php?mailsend");
-
-
+    header("Location: Contact.php?mailsend=success");
+    exit();
 }
+?>
